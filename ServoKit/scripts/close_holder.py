62b5f87019b38d6d941de5f60a23c4b8127df16f
Oleksandr Skachkov <gskachkov@gmail.com>
2020-02-03 00:52:14 +0200

    Prepare to submit

--- ServoKit/scripts/close_holder.py
+++ ServoKit/scripts/close_holder.py
@@ -1,1 +1,8 @@
 #close holder
+from adafruit_servokit import ServoKit
+
+# Set channels to the number of servo channels on your kit.
+# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
+kit = ServoKit(channels=8)
+
+kit.servo[0].angle = 40
